b02feafe0cde4b519a2ccf797c2759a1334040dd
SquallB <mazento2002@yaho.fr>
2014-10-02 17:24:10 +0200

    ajout de classes pour la gestion de bdd

---
+++ media/stephane/A6FE52C1FE528A07/ISEN/M1/Android/GithubDepot/FIlmList/src/fr/isen/android/filmlist/DatabaseHandler.java
@@ -0,0 +1,33 @@
+package fr.isen.android.filmlist;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+	  public static final String FILM_KEY = "id";
+	  public static final String FILM_NAME = "name";
+	    
+	  public static final String FILM_TABLE_NAME = "film";
+	  public static final String FILM_TABLE_CREATE =
+	    "CREATE TABLE " + FILM_TABLE_NAME + " (" +
+	    		FILM_KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+	    		FILM_NAME + " TEXT);";
+
+	  public DatabaseHandler(Context context, String name, CursorFactory factory, int version) {
+	    super(context, name, factory, version);
+	  }
+
+	  @Override
+	  public void onCreate(SQLiteDatabase db) {
+	    db.execSQL(FILM_TABLE_CREATE);
+	  }
+	  
+	  public static final String FILM_TABLE_DROP = "DROP TABLE IF EXISTS " + FILM_TABLE_NAME + ";";
+	  @Override
+	  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+	    db.execSQL(FILM_TABLE_DROP);
+	    onCreate(db);
+	  }
+	}
