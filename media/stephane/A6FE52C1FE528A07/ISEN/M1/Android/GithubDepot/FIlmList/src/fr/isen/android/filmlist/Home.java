020a7120d9f1b8bb2e4fdd8d7a8d339590d95f36
Scay <stephane.viper@gmail.com>
2014-10-07 21:48:03 +0200

    Ajout du bouton de partage.

--- media/stephane/A6FE52C1FE528A07/ISEN/M1/Android/GithubDepot/FIlmList/src/fr/isen/android/filmlist/Home.java
+++ media/stephane/A6FE52C1FE528A07/ISEN/M1/Android/GithubDepot/FIlmList/src/fr/isen/android/filmlist/Home.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
@@ -22,6 +23,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
+import android.widget.ShareActionProvider;
 
 import com.example.filmlist.R;
 
@@ -33,18 +35,20 @@ public class Home extends Activity {
 	private ActionBarDrawerToggle drawerToggle;
 	private CharSequence drawerTitle;
 	private CharSequence mTitle;
-	
+
 	private FilmDAO dao;
 	private ArrayList<String> list;
-	private ArrayAdapter adapter; 
-	
+	private ArrayAdapter adapter;
+
+	private ShareActionProvider mShareActionProvider;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 
 		this.dao = new FilmDAO(this);
-		
+
 		// Initialisation du "tiroir"
 		navigationArray = getResources().getStringArray(
 				R.array.navigation_array);
@@ -58,30 +62,26 @@ public class Home extends Activity {
 		drawerList.setOnItemClickListener(new DrawerItemClickListener());
 
 		mTitle = drawerTitle = getTitle();
-		
 
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setHomeButtonEnabled(true);
-		
-		drawerToggle = new ActionBarDrawerToggle(this,
-		drawerLayout, 
-		R.drawable.ic_drawer, 
-		R.string.drawer_open, 
-		R.string.drawer_close 
-		) {
+
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close) {
 
 			/** Called when a drawer has settled in a completely closed state. */
 			public void onDrawerClosed(View view) {
 				super.onDrawerClosed(view);
 				getActionBar().setTitle(mTitle);
-				invalidateOptionsMenu(); 
+				invalidateOptionsMenu();
 			}
 
 			/** Called when a drawer has settled in a completely open state. */
 			public void onDrawerOpened(View drawerView) {
 				super.onDrawerOpened(drawerView);
 				getActionBar().setTitle(drawerTitle);
-				invalidateOptionsMenu(); 
+				invalidateOptionsMenu();
 			}
 		};
 
@@ -91,65 +91,61 @@ public class Home extends Activity {
 		dao.open();
 		final List<Film> films = dao.getAllFilms();
 		dao.close();
-		
-	    final ListView listview = (ListView) findViewById(R.id.listview);
 
-	    list = new ArrayList<String>();
-	    
-	    for(Film film : films) {
-	    	list.add(film.getName());
-	    }
-	    adapter = new ArrayAdapter<String>(this,
-	        android.R.layout.simple_list_item_1, list);
-	    listview.setAdapter(adapter);
-	    
-	    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		final ListView listview = (ListView) findViewById(R.id.listview);
 
-	      @Override
-	      public void onItemClick(AdapterView<?> parent, final View view,
-	          int position, long id) {
-	        final String item = (String) parent.getItemAtPosition(position);
-	        view.animate().setDuration(2000).alpha(0)
-	        .withEndAction(new Runnable() {
-	              @Override
-	              public void run() {
-	            	dao.open();
-	            	dao.delete(item);
-	            	dao.close();
-	                list.remove(item);
-	                adapter.notifyDataSetChanged();
-	                view.setAlpha(1);
-	              }
-	            });
-	      }
+		list = new ArrayList<String>();
 
-	    });
+		for (Film film : films) {
+			list.add(film.getName());
+		}
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+		listview.setAdapter(adapter);
+
+		/*
+		 * listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+		 * {
+		 * 
+		 * @Override public void onItemClick(AdapterView<?> parent, final View
+		 * view, int position, long id) { final String item = (String)
+		 * parent.getItemAtPosition(position);
+		 * view.animate().setDuration(2000).alpha(0) .withEndAction(new
+		 * Runnable() {
+		 * 
+		 * @Override public void run() { dao.open(); dao.delete(item);
+		 * dao.close(); list.remove(item); adapter.notifyDataSetChanged();
+		 * view.setAlpha(1); } }); }
+		 * 
+		 * });
+		 */
 
 	}
 
-	public void handleSearch(String query){
+	public void handleSearch(String query) {
 		selectItem(2);
 	}
-	
+
 	@Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
-		
+
 		// Ajout du listener sur la barre de recherche
-		final SearchView searchView=(SearchView)menu.findItem(R.id.action_search).getActionView();
+		final SearchView searchView = (SearchView) menu.findItem(
+				R.id.action_search).getActionView();
 		final MenuItem searchMenuItem = menu.findItem(R.id.action_search);
-		searchView.setOnQueryTextListener(new OnQueryTextListener(){
+		searchView.setOnQueryTextListener(new OnQueryTextListener() {
 
 			@Override
 			public boolean onQueryTextChange(String newText) {
-				
+
 				return true;
 			}
 
@@ -161,30 +157,38 @@ public class Home extends Activity {
 				list.add(film.getName());
 				adapter.notifyDataSetChanged();
 				searchMenuItem.collapseActionView();
-                searchView.setQuery("", false);
+				searchView.setQuery("", false);
 				return true;
 			}
-			
-			
 		});
+
+		// Localisation du menu item avec  ShareActionProvider
+		MenuItem item = menu.findItem(R.id.action_share);
+		// Ajout du listener
+		mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+		mShareActionProvider.setShareIntent(getDefaultShareIntent());
+		
 		return true;
 	}
-	
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (drawerToggle.onOptionsItemSelected(item)) {
-          return true;
-        }
-        if(R.id.action_search == item.getItemId()){
-        	//selectItem(2);
-        }
-        // Handle your other action bar items...
 
-        return super.onOptionsItemSelected(item);
-    }
+	private Intent getDefaultShareIntent() {
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_SUBJECT, "SUBJECT");
+		intent.putExtra(Intent.EXTRA_TEXT, "Extra Text");
+		return intent;
+	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (drawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 
 	private class DrawerItemClickListener implements
 			ListView.OnItemClickListener {
@@ -194,10 +198,10 @@ public class Home extends Activity {
 			selectItem(position);
 		}
 	}
-	
+
 	public void setTitle(CharSequence title) {
-	    mTitle = title;
-	    getActionBar().setTitle(mTitle);
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
 	}
 
 	/** Swaps fragments in the main content view */
@@ -218,13 +222,14 @@ public class Home extends Activity {
 	}
 
 	/* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
-        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// If the nav drawer is open, hide action items related to the content
+		// view
+		boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+		menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
 
 	public static class PlaceholderFragment2 extends Fragment {
 
@@ -239,36 +244,36 @@ public class Home extends Activity {
 			return rootView;
 		}
 	}
-	
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        drawerToggle.syncState();
-    }
 
-    private class StableArrayAdapter extends ArrayAdapter<String> {
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		drawerToggle.syncState();
+	}
 
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+	private class StableArrayAdapter extends ArrayAdapter<String> {
 
-        public StableArrayAdapter(Context context, int textViewResourceId,
-            List<String> objects) {
-          super(context, textViewResourceId, objects);
-          for (int i = 0; i < objects.size(); ++i) {
-            mIdMap.put(objects.get(i), i);
-          }
-        }
+		HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
 
-        @Override
-        public long getItemId(int position) {
-          String item = getItem(position);
-          return mIdMap.get(item);
-        }
+		public StableArrayAdapter(Context context, int textViewResourceId,
+				List<String> objects) {
+			super(context, textViewResourceId, objects);
+			for (int i = 0; i < objects.size(); ++i) {
+				mIdMap.put(objects.get(i), i);
+			}
+		}
 
-        @Override
-        public boolean hasStableIds() {
-          return true;
-        }
+		@Override
+		public long getItemId(int position) {
+			String item = getItem(position);
+			return mIdMap.get(item);
+		}
 
-      }
+		@Override
+		public boolean hasStableIds() {
+			return true;
+		}
+
+	}
 }
