1651584514df790b48fb2c3cd8139d46e2ec5289
ScaY <stephane.viper@gmail.com>
2014-11-06 13:51:08 +0100

    Removed comments

--- media/stephane/A6FE52C1FE528A07/ISEN/M1/Android/GithubDepot/FIlmList/src/fr/isen/android/filmlist/bdd/DAOBase.java
+++ media/stephane/A6FE52C1FE528A07/ISEN/M1/Android/GithubDepot/FIlmList/src/fr/isen/android/filmlist/bdd/DAOBase.java
@@ -4,29 +4,25 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 public abstract class DAOBase {
-	  // Nous sommes à la première version de la base
-	  // Si je décide de la mettre à jour, il faudra changer cet attribut
-	  protected final static int VERSION = 7;
-	  // Le nom du fichier qui représente ma base
-	  protected final static String NOM = "film_list.db";
-	    
-	  protected SQLiteDatabase mDb = null;
-	  protected DatabaseHandler mHandler = null;
-	    
-	  public DAOBase(Context pContext) {
-	    this.mHandler = new DatabaseHandler(pContext, NOM, null, VERSION);
-	  }
-	    
-	  public void open() {
-	    // Pas besoin de fermer la dernière base puisque getWritableDatabase s'en charge
-	    mDb = mHandler.getWritableDatabase();
-	  }
-	    
-	  public void close() {
-	    mDb.close();
-	  }
-	    
-	  public SQLiteDatabase getDb() {
-	    return mDb;
-	  }
+	protected final static int VERSION = 7;
+	protected final static String NOM = "film_list.db";
+
+	protected SQLiteDatabase mDb = null;
+	protected DatabaseHandler mHandler = null;
+
+	public DAOBase(Context pContext) {
+		this.mHandler = new DatabaseHandler(pContext, NOM, null, VERSION);
 	}
+
+	public void open() {
+		mDb = mHandler.getWritableDatabase();
+	}
+
+	public void close() {
+		mDb.close();
+	}
+
+	public SQLiteDatabase getDb() {
+		return mDb;
+	}
+}
