72fbcf6a196753e047922a3ed8070b1333cf11c7
Scay <stephane.viper@gmail.com>
2014-11-10 19:49:46 +0100

    Modification Watch en Wtach

--- media/stephane/A6FE52C1FE528A07/ISEN/M1/Android/GithubDepot/FIlmList/src/fr/isen/android/filmlist/ui/FilmToSeeListFragment.java
+++ media/stephane/A6FE52C1FE528A07/ISEN/M1/Android/GithubDepot/FIlmList/src/fr/isen/android/filmlist/ui/FilmToSeeListFragment.java
@@ -12,7 +12,7 @@ public class FilmToSeeListFragment extends FilmListFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		getActivity().setTitle("Wtach movies");
+		getActivity().setTitle("Watch movies");
 		String typeKey = this.getClass().getSimpleName().toString();
 		View view = super.onCreateView(inflater, container, savedInstanceState);
 		additemListener(getListView(), typeKey.toString(), getList(),
